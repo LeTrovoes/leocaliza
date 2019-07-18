f78f8dd83548eee862708dad639d303379472202
Leandro Trovões <leandrotrovoes@hotmail.com>
2019-07-17 23:39:18 -0300

    Initial Commit

---
+++ src/main/java/br/edu/ufabc/leocaliza/LeocalizaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufabc.leocaliza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeocalizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeocalizaApplication.class, args);
+	}
+
+}
