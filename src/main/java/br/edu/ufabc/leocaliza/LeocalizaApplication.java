a670f78c9e5ab2892a3342106dd5955d4ebae5ba
Leo <leonardojcarmo@hotmail.com>
2019-07-26 16:14:01 -0300

    fix: turn the project into a non-web application

--- src/main/java/br/edu/ufabc/leocaliza/LeocalizaApplication.java
+++ src/main/java/br/edu/ufabc/leocaliza/LeocalizaApplication.java
@@ -1,13 +1,19 @@
 package br.edu.ufabc.leocaliza;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LeocalizaApplication {
+public class LeocalizaApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(LeocalizaApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+
   }
 
 }
