d7361b513f322fcb32fb995d5140963406fe8311
Leo <leonardojcarmo@hotmail.com>
2019-07-26 16:14:55 -0300

    fix: repair database access problems

--- src/main/java/br/edu/ufabc/leocaliza/entity/User.java
+++ src/main/java/br/edu/ufabc/leocaliza/entity/User.java
@@ -1,20 +1,28 @@
 package br.edu.ufabc.leocaliza.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
 import org.hibernate.annotations.NaturalId;
+import org.springframework.lang.NonNull;
 
 @Entity
+@Table(name="leocalizaUser")
 @Inheritance(strategy = InheritanceType.JOINED)
 public abstract class User {
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
   @NaturalId
+  @Column(unique = true)
+  @NonNull
   protected String cpf;
 
 }
