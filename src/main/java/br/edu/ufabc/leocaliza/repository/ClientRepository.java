f367231cddf87d79c03d2caca7029456066c1bf9
Leo <leonardojcarmo@hotmail.com>
2019-07-30 21:14:53 -0300

    feat: add repositories

---
+++ src/main/java/br/edu/ufabc/leocaliza/repository/ClientRepository.java
@@ -0,0 +1,12 @@
+package br.edu.ufabc.leocaliza.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ufabc.leocaliza.entity.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+  List<Client> findByCpf(String cpf);
+}
--- src/main/java/br/edu/ufabc/leocaliza/repository/VehicleRepository.java
+++ src/main/java/br/edu/ufabc/leocaliza/repository/VehicleRepository.java
@@ -1,10 +1,11 @@
 package br.edu.ufabc.leocaliza.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.edu.ufabc.leocaliza.entity.Filial;
+import br.edu.ufabc.leocaliza.entity.Vehicle;
 
 @Repository
-public interface FilialRepository extends JpaRepository<Filial, Long> {
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 }
