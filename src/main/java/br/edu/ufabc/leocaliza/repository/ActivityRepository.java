3e6b18056420c3e01d46df3ef8c323d8da1534aa
Leandro Trovões <leandrotrovoes@hotmail.com>
2019-08-04 01:06:54 -0300

    feat: add activity entity repository and service

--- src/main/java/br/edu/ufabc/leocaliza/repository/ActivityRepository.java
+++ src/main/java/br/edu/ufabc/leocaliza/repository/ActivityRepository.java
@@ -4,8 +4,20 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import br.edu.ufabc.leocaliza.entity.Activity;
 import br.edu.ufabc.leocaliza.entity.Branch;
+import br.edu.ufabc.leocaliza.entity.User;
+import br.edu.ufabc.leocaliza.entity.Vehicle;
 
 @Repository
-public interface ActivityRepository extends JpaRepository<Branch, Long> {
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
+  public List<Activity> findByUser(User user);
+
+  public List<Activity> findByVehicle(Vehicle vehicle);
+
+  public List<Activity> findByStartingBranch(Branch branch);
+
+  public List<Activity> findByEndingBranch(Branch branch);
+
 }
