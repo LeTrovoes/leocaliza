af121c13f20d997dc7aba4bef488c75554ee8f51
Leo <leonardojcarmo@hotmail.com>
2019-08-03 23:08:40 -0300

    feat: merge MainGui with LeocalizaApplication and add usage of fxml

---
+++ src/main/java/br/edu/ufabc/leocaliza/controller/LoginController.java
@@ -0,0 +1,14 @@
+package br.edu.ufabc.leocaliza.controller;
+
+import org.springframework.stereotype.Controller;
+
+import javafx.fxml.FXML;
+
+@Controller
+public class LoginController{
+  @FXML
+  private void teste()
+  {
+      System.out.println("Works");
+  }
+}
